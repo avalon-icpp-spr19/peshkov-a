b3744250298e8921894f2168d73f3d38c00b5b45
Dragonpemp <47276660+Dragonpemp@users.noreply.github.com>
2019-04-13 00:35:25 +0300

    Add files via upload

--- Hometask7.cpp
+++ Hometask7.cpp
@@ -2,10 +2,10 @@
 #include<cmath>
 #include <string>
 #include <limits>
-//#include "ext.hpp"
+#include "Extention.hpp"
 using namespace std;
-//using namespace ext;
-
+using namespace ext;
+const int MAX_INPUT_SIZE = 256;
 int main()
 {
 	/*
@@ -21,7 +21,6 @@ int main()
 	cout << ptr << endl;
 	cout << *ptr << endl;
 	Пусть объявлены следующие переменные:
-
 	Объявите и проинициализируйте указатели на вышеобъявленные переменные.
 	Какой размер имеют объявленные указатели? Проверьте с помощью оператора sizeof
 	С помощью объявленных указателей:
@@ -39,21 +38,16 @@ int main()
 	cout << sizeof(ptr1) << endl;
 	cout << ptr1 << endl;
 	cout << *ptr1 << endl;
-
 	ptr2 = &b;
 	cout << sizeof(ptr2) << endl;
 	cout << ptr2 << endl;
 	cout << *ptr2 << endl;
-
 	cout << sizeof(ptr3) << endl;
 	cout << ptr3 << endl;
 	cout << *ptr3 << endl;
-
 	cout << sizeof(ptr4) << endl;
 	cout << ptr4 << endl;
 	cout << *ptr4 << endl;
-
-
 	a = *ptr1 + 1;
 	b = *ptr2 * 0.3125;
 	cout << *ptr3 * 2 << endl;
@@ -61,29 +55,33 @@ int main()
 	Задание №2
 	Что выведет на экран следующий код?
 	Прокоментируйте каждую строку, запишите промежуточные значения переменных.
-	
 
 	int t[4] = {8,4,2,1};
 	int *p1 = t + 2//элемент номер 2 (значение:2)
 		, *p2 = p1 - 1;//элемент номер 1 (значение:4)
 	p1++;//элемент номер 3 (значение:1)
 	cout << *p1 - t[p1 - p2] << endl;//1-t[номер 3-номер 1]=1-2=-1
-	
-	
+
+
 	int data[] = {1,2,3,4,5};
 	Объявите указатель на первый элемент массива*/
 
-	int data[] = { 1,2,3,4,5 };
-int *ptr=data;
-for (int i = 0; i < 5; i++)
-{
-	cout << *(ptr+i) << endl;
-}
-for (int j = 0; j < 5; j++)
-{
-	data[j] = *(ptr + j) * 2;
-		cout <<  data[j] << endl;
-}
+	/*int data[] = { 1,2,3,4,5 },sum=0;
+	int *ptr = data;
+	for (int i = 0; i < 5; i++)
+	{
+		cout << *(ptr + i) << endl;
+	}
+	for (int j = 0; j < 5; j++)
+	{
+		data[j] = *(ptr + j) * 2;
+		cout << data[j] << endl;
+	}
+	for (int k = 0; k < 5; k++)
+	{
+		sum = *(ptr + k) +sum;
+	}
+	cout << sum << endl;
 	/*С помощью цикла for и объвленного указателя выведите каждый элемент массива в консоль.
 	С помощью цикла for и объвленного указателя увеличьте каждый элемент массива в 2 раза
 	и посчитайте сумму получившихся элементов массива.
@@ -95,6 +93,22 @@ for (int j = 0; j < 5; j++)
 	4.	Используя дополнительный указатель найдите максимальный элемент массива.
 	5.	Выведите его индекс и значение. (Для вывода индекса использовать арифметику указателей).
 	При обходе массива использовать указатели, запрещено обращаться к элементам массива по индексам.
+		int mass[20];
+	int *ptr = mass;
+	int *max = mass;
+	for (int i = 0; i < 20; i++) mass[i] = GetRandomValue(-100, 100);
+
+	for (int i = 0; i < 20; i++)
+	{
+		cout << *(ptr + i) << " ";
+	}
+	cout << endl;
+
+	for (int i = 0; i < 20; i++)
+	{
+		if (*max < *(mass + i)) max = mass + i;
+	}
+	cout << (max - mass) << ": " << *max << endl;
 	Задание №4
 	Пусть задан некоторый символьный массив, проинициализированный строковым литералом.
 	Например const char data[] = "abcdefghijklmnopqrstuvwxyz";
@@ -105,6 +119,21 @@ for (int j = 0; j < 5; j++)
 	d				4
 	z				26
 	!				-1
+		const char data[] = "abcdefghijklmnopqrstuvwxyz";
+	const char *ptr = data;
+	char x;
+	int i = -1;
+	cin >> x;
+	while (*ptr != '\0')
+	{
+		if (x == *ptr)
+		{
+			i = (ptr - data) + 1;
+			break;
+		}
+		ptr++;
+	}
+	cout << i << endl;
 	Задание №5
 	Написать программу, которая сравнивает две строки, которые вводит пользователь.
 	Сравнение реализовать посимвольно.
@@ -117,6 +146,34 @@ for (int j = 0; j < 5; j++)
 	{
 	char line[256];
 	cin.getline(line, 256);
+	}
+	bool i = false;
+	char line1[MAX_INPUT_SIZE];
+	char line2[MAX_INPUT_SIZE];
+	cin.getline(line1, MAX_INPUT_SIZE);
+	cin.getline(line2, MAX_INPUT_SIZE);
+	char * ptr1 = line1;
+	char * ptr2 = line2;
+	while ((*ptr1 != '\0') || (*ptr2 != '\0'))
+	{
+		if (*ptr1 == *ptr2)
+		{
+			i = true;
+		}
+		else
+		{
+			i = false;
+		}
+		++ptr1;
+		++ptr2;
+	}
+	if (i)
+	{
+		cout << 1 << endl;
+	}
+	else
+	{
+		cout << 0 << endl;
 	}
 	*/
 }
